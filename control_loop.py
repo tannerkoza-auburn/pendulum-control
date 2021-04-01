e648d1836820283047ca118252ff628bb658de4f
Tanner Koza <48143385+tannerkoza@users.noreply.github.com>
2021-04-01 13:09:33 -0500

    Cleaned code and corrected filter readings.

--- control_loop.py
+++ control_loop.py
@@ -12,13 +12,13 @@ adc.start_adc(0, gain=GAIN)  # change to corresponding input channel
 
 # Define Sampling Frequency
 fs = 21
-T = 1/fs  # this defines the time between samples
+T = 1/fs  # defines sample period
 
 # Define Control Output
 controlPIN = 0
 
 # Define ADC Voltage Range & Max Value
-range_adc = 4.096
+range_adc = 6.144
 max_adc = 32767
 
 # Define Control Gains
@@ -31,7 +31,7 @@ A = Kp + ((2*Kd)/T) + ((T*Ki)/2)
 B = (T*Ki) - ((4*Kd)/T)
 C = ((2*Kd)/T) + ((T*Ki)/2) - Kp
 
-# Define Filter z-Transform Coefficients
+# Define Filter 2nd Order z-Transform Coefficients
 a = 0.0002862
 b = 0.0005723
 c = 0.8219
@@ -49,19 +49,20 @@ lf_in = 0
 llf_out = 0
 llf_in = 0
 
-# Moving Average
+# Define Moving Average Loop Value
 l_sig = 0
 
 # Define Logging Lists
 pos_values = list()
 sig_values = list()
+filt_sig_values = list()
 
 
-# Define Method for Set Point Definition
+# Define Function for Set Point Definition
 def set_point(rng, mx):
     input('Place Pendulum to Desired Set Point and Press ENTER.')
-    dig_pos = adc.get_last_result()
-    sp = rng/(mx/dig_pos)  # initial analog voltage
+    dig = adc.get_last_result()
+    sp = rng/(mx/dig)  # initial analog voltage
     return sp
 
 
@@ -73,25 +74,34 @@ def loop_rate(period, t):
             break
 
 
+# Define Set Point
 des = set_point(range_adc, max_adc)
 input('\nStart Control?')
 
+# Control Loop
 try:
     while True:
-        now = time.time()
+        now = time.time()   # sets time at start of the loop to maintain loop rate
         dig_pos = adc.get_last_result()   # retrieves last position voltage value from potentiometer
-        filt_pos = a*dig_pos + b*lf_in + a*llf_in + c*lf_out - d*llf_out
+
+        filt_pos = a*dig_pos + b*lf_in + a*llf_in + c*lf_out - d*llf_out    # filters digital position data
+
         if dig_pos != 0:
-            pos = range_adc/(max_adc/dig_pos)  # determines analog voltage from digital voltage
+            pos = range_adc/(max_adc/filt_pos)  # determines analog voltage from filtered digital voltage value
         else:
-            pos = dig_pos
+            pos = filt_pos   # sets analog voltage to 0 if digital voltage is 0
             
-        pos_values.append(pos)
+        pos_values.append(pos)  # logs filtered position
+
         error = des - pos   # defines error
-        control_sig = l_l_control + A*error + B*l_error + C*l_l_error
-        avg_sig = (l_sig + control_sig)/2
-        sig_values.append(avg_sig)
-        # Insert Output Code Here
+        control_sig = l_l_control + A*error + B*l_error + C*l_l_error   # determines control signal
+        avg_sig = (l_sig + control_sig)/2   # determines moving average of control signal to output
+        sig_values.append(control_sig)
+        filt_sig_values.append(avg_sig)
+
+        # Insert Output Code Here (avg_sig)
+
+        # Variable Management
         l_l_error = l_error
         l_error = error
         l_l_control = l_control
@@ -103,14 +113,17 @@ try:
         lf_in = dig_pos
         
         l_sig = control_sig
+
+        # Loop Rate Function
         loop_rate(T, now)
+
 except KeyboardInterrupt:
     sp_values = [des]*len(pos_values)
     data = pd.DataFrame(pos_values, columns=['Position Voltage'])
     data['Set Point'] = sp_values
     data['Commanded Voltage'] = sig_values
+    data['Filtered Commanded Voltage'] = filt_sig_values
     data['Time'] = data.index/fs
     data.to_csv('data.csv')
-    fig = px.line(data, x='Time', y=['Position Voltage', 'Commanded Voltage', 'Set Point'])
+    fig = px.line(data, x='Time', y=['Position Voltage', 'Set Point', 'Commanded Voltage', 'Filtered Commanded Voltage'])
     fig.show()
-
