06881eb58aa6990add64d45329a06e1ad950ff45
Tanner Koza <kozatanner@gmail.com>
2021-03-31 17:10:51 -0900

    Logging and Control changes were made.

--- control_loop.py
+++ control_loop.py
@@ -1,7 +1,9 @@
 import time
+import Adafruit_ADS1x15
 import RPi.GPIO as GPIO
 import pandas as pd
-import Adafruit_ADS1x15
+import plotly.express as px
+
 
 # Define the ADC Instance
 adc = Adafruit_ADS1x15.ADS1115()
@@ -9,15 +11,19 @@ GAIN = 2/3
 adc.start_adc(0, gain=GAIN)  # change to corresponding input channel
 
 # Define Sampling Frequency
-fs = 60
+fs = 100
 T = 1/fs  # this defines the time between samples
 
-# Define Control Input/Output
+# Define Control Output
+controlPIN = 0
 
+# Define ADC Voltage Range & Max Value
+range_adc = 4.096
+max_adc =  32767 
 
 # Define Control Gains
-Kp = 0  # proportional gain
-Kd = 0  # derivative gain
+Kp = 0.2002  # proportional gain
+Kd = 0.0138  # derivative gain
 Ki = 0  # integral gain
 
 # Define z-Transform Coefficients
@@ -31,14 +37,16 @@ l_l_error = 0
 l_control = 0
 l_l_control = 0
 
-# Define Logging List
+# Define Logging Lists
 pos_values = list()
+sig_values = list()
 
 
 # Define Method for Set Point Definition
-def set_point():
+def set_point(rng, mx):
     input('Place Pendulum to Desired Set Point and Press ENTER.')
-    sp = adc.get_last_result()
+    dig_pos = adc.get_last_result()
+    sp = rng/(mx/dig_pos)  # initial analog voltage
     return sp
 
 
@@ -51,13 +59,21 @@ def loop_rate(period):
             break
 
 
-des = set_point()
+des = set_point(range_adc, max_adc)
+input('\nStart Control?')
+
 try:
     while True:
-        pos = adc.get_last_result()   # retrieves last position voltage value from potentiometer
+        dig_pos = adc.get_last_result()   # retrieves last position voltage value from potentiometer
+        if dig_pos != 0:
+            pos = range_adc/(max_adc/dig_pos)  # determines analog voltage from digital voltage
+        else:
+            pos = dig_pos
+            
         pos_values.append(pos)
         error = des - pos   # defines error
         control_sig = l_l_control + A*error + B*l_error + C*l_l_error
+        sig_values.append(control_sig)
         # Insert Output Code Here
         l_l_error = l_error
         l_error = error
@@ -65,6 +81,10 @@ try:
         l_control = control_sig
         loop_rate(T)
 except KeyboardInterrupt:
-    data = pd.DataFrame(pos_values)
+    data = pd.DataFrame(pos_values, columns=['Position Voltage'])
+    data['Commanded Voltage'] = sig_values
+    data['Time'] = data.index/fs
     data.to_csv('data.csv')
+    fig = px.line(data, x='Time', y=['Position Voltage', 'Commanded Voltage'])
+    fig.show()
 
